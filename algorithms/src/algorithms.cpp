9371b8910827a305ed4b10f9a9e1c92cf2de62c5
philip <philip.herzl@outloo.com>
2024-11-03 22:06:41 +0100

    Initial commit

---
+++ algorithms/src/algorithms.cpp
@@ -0,0 +1,7 @@
+//#include <iostream>
+#include <fmt/format.h>
+
+void algorithms(){
+    fmt::print("I am the {} component!", "algorithms");
+    //std::cout << "I am the algorithms component!\n";
+}
---
+++ test_package/src/example.cpp
@@ -0,0 +1,11 @@
+#include <algorithms/algorithms.h>
+#include <network/network.h>
+#include <ai/ai.h>
+#include <rendering/rendering.h>
+
+int main() {
+    algorithms();
+    network();
+    ai();
+    rendering();
+}
