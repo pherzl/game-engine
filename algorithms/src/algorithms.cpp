f66faa084a73b4b37897b79686602f643a0e716a
philip <philip.herzl@outloo.com>
2024-11-04 18:50:12 +0100

    added zlib from first conan tutorial

--- algorithms/src/algorithms.cpp
+++ algorithms/src/algorithms.cpp
@@ -1,7 +1,35 @@
-//#include <iostream>
+#include <iostream>
 #include <fmt/format.h>
+#include <zlib.h>
 
 void algorithms(){
-    fmt::print("I am the {} component!", "algorithms");
-    //std::cout << "I am the algorithms component!\n";
+    std::cout << "I am the algorithms component!\n";
+
+    //fmt::print("I am the {} component!", "algorithms");
+    //auto str = fmt::format("I am the algorithms component!\n");
+    //std::cout << str << '\n';
+
+    char buffer_in [256] = {"Conan is a MIT-licensed, Open Source package manager for C and C++ development "
+                            "for C and C++ development, allowing development teams to easily and efficiently "
+                            "manage their packages and dependencies across platforms and build systems."};
+    char buffer_out [256] = {0};
+    z_stream defstream;
+    defstream.zalloc = Z_NULL;
+    defstream.zfree = Z_NULL;
+    defstream.opaque = Z_NULL;
+    defstream.avail_in = (uInt) strlen(buffer_in);
+    defstream.next_in = (Bytef *) buffer_in;
+    defstream.avail_out = (uInt) sizeof(buffer_out);
+    defstream.next_out = (Bytef *) buffer_out;
+
+    deflateInit(&defstream, Z_BEST_COMPRESSION);
+    deflate(&defstream, Z_FINISH);
+    deflateEnd(&defstream);
+
+    printf("Uncompressed size is: %lu\n", strlen(buffer_in));
+    printf("Compressed size is: %lu\n", strlen(buffer_out));
+
+    printf("ZLIB VERSION: %s\n", zlibVersion());
+
+
 }
