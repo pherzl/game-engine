de971657fe3ade44a165379ab6433ff483f5d40f
Philip Herzl <philip.herzl@kestrel-eye.com>
2024-11-05 21:10:33 +0100

    fixes the linker errors

--- ai/src/ai.cpp
+++ ai/src/ai.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include <fmt/format.h>
 #include <algorithms/algorithms.h>
 
 void ai(){
     std::cout << "I am the ai component!\n";
+    fmt::println("I am the {} component!", "ai");
     algorithms();
 }
